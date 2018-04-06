7798be0a2d36760ac2e83514885f84190f6e37d5
roger <rogerul_2000@hotmail.com>
2018-04-06 10:31:48 -0500

    Responsive tablet 100%

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
-    $('.autoplay').slick({
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
     // Navs responsive
