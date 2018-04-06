8de24de74cc85e91d4320bde5e28d338a287d1b1
roger <rogerul_2000@hotmail.com>
2018-04-06 13:19:07 -0500

    Responsive mobile 70%

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $('.responsive').slick({
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 2000,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -21,7 +21,7 @@ $(document).ready(function() {
                 breakpoint: 769,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
@@ -31,9 +31,6 @@ $(document).ready(function() {
                     slidesToScroll: 1
                 }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ]
     });
 
