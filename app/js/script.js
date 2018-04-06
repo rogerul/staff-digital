24b3d01e5f65e09d13c50ff0d5499c253702b519
roger <rogerul_2000@hotmail.com>
2018-04-06 02:54:25 -0500

    Maquetacion menu responsive 100%

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    // Navs responsive
+    $(function(){
+        $('#menu').slicknav();
+    });
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("app/css/**/*.css").on('change', browserSync.reload);
     gulp.watch("app/*.html").on('change', browserSync.reload);
     gulp.watch("app/js/*.js").on('change', browserSync.reload);
 });
